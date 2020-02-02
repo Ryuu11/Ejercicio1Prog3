808a4c8e9d261cf3ba8cf43b7a4500311176102b
Ryuu11 <60558023+Ryuu11@users.noreply.github.com>
2020-02-02 17:19:55 -0400

    Ejercicio 1

---
+++ Ejercicio1prog3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ejercicio1prog3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe tu numero ");
+
+            int numero, invertido, decenas, unidades;
+
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            decenas = numero / 10;
+            unidades = numero % 10;
+            invertido = (unidades * 10) + decenas;
+
+            Console.WriteLine("Su numero invertido es:" + invertido);
+        }
+    }
+}
